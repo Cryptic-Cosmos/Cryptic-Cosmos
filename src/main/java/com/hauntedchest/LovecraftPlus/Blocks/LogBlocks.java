bdc5499db14e5510968671fd065dcf4fc58c99de
PutoPug <muralihrishikesh@gmail.com>
2020-10-13 22:19:01 +0530

    Thorn Planks & Thorn Log Is Done But I Am Dead On The Inside(PLSS WORK)

--- src/main/java/com/hauntedchest/LovecraftPlus/Blocks/LogBlocks.java
+++ src/main/java/com/hauntedchest/LovecraftPlus/Blocks/LogBlocks.java
@@ -1,13 +1,24 @@
 package com.hauntedchest.LovecraftPlus.Blocks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.LogBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class LogBlocks extends LogBlock {
-    public LogBlocks() {
+    public LogBlocks(Properties builder) {
+
+
         super(MaterialColor.WOOD, Properties.create(Material.WOOD)
                         .hardnessAndResistance(1.0F, 1.0F)
                         .sound(SoundType.WOOD)
@@ -15,6 +26,15 @@ public class LogBlocks extends LogBlock {
                         .harvestTool(ToolType.AXE)
         );
 
-        ;
+
     }
+    protected static final VoxelShape collision_shape = Block.makeCuboidShape(1D, 0D, 1D, 17D, 18D, 17D);
+    @Override
+    public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+        entityIn.attackEntityFrom(DamageSource.CACTUS, 2.0F);
+    }
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return collision_shape;
+    }
+
 }
