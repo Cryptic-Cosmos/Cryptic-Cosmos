c6b51a3e87d0f719c23de211550a0c6b4aa47314
PutoPug <muralihrishikesh@gmail.com>
2020-10-13 21:28:43 +0530

    Thorn Log&& Thorn Planks ===== DONE!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/com/hauntedchest/LovecraftPlus/Inits/BlockHandeler.java
+++ src/main/java/com/hauntedchest/LovecraftPlus/Inits/BlockHandeler.java
@@ -4,8 +4,10 @@ import com.hauntedchest.LovecraftPlus.Blocks.*;
 import com.hauntedchest.LovecraftPlus.LovecraftPlusMod;
 import com.hauntedchest.LovecraftPlus.world.feature.ThornTree;
 import net.minecraft.block.*;
+import com.hauntedchest.LovecraftPlus.Blocks.LogBlocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -30,8 +32,8 @@ public class BlockHandeler {
     //Wood Blocks
 
     //Thorn Wood
-    public static final RegistryObject<Block> THORN_LOG = BLOCKS.register("thorn_log",() -> new LogBlock(MaterialColor.WOOD,Block.Properties.from(Blocks.JUNGLE_LOG)));
-    public static final RegistryObject<Block> THORN_PLANKS = BLOCKS.register("thorn_planks",() -> new Block(Block.Properties.from(Blocks.JUNGLE_PLANKS)));
+    public static final RegistryObject<Block> THORN_LOG = BLOCKS.register("thorn_log",() -> new LogBlocks(Block.Properties.create(Material.WOOD).hardnessAndResistance(1.0F, 1.0F).sound(SoundType.WOOD).harvestLevel(0).harvestTool(ToolType.AXE)));
+    public static final RegistryObject<Block> THORN_PLANKS = BLOCKS.register("thorn_planks",() -> new LogBlocks(Block.Properties.create(Material.WOOD).hardnessAndResistance(1.0F, 1.0F).sound(SoundType.WOOD).harvestLevel(0).harvestTool(ToolType.AXE)));
     public static final RegistryObject<Block> THORN_SAPLING = BLOCKS.register("thorn_sapling",
             () -> new ThornSapling(ThornTree::new, Block.Properties.from(Blocks.JUNGLE_SAPLING)));
     public static final RegistryObject<Block> THORN_LEAVES = BLOCKS.register("thorn_leaves",() -> new LeavesBlock(Block.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly().sound(SoundType.PLANT).notSolid()));
