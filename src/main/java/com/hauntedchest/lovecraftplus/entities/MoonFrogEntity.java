0049da9efcb23169a7cc37814f0f329389fc9fc1
DenDragon23 <denyswriter@gmail.com>
2020-11-04 19:23:01 -0800

    moon frogge ai not workke

--- src/main/java/com/hauntedchest/lovecraftplus/entities/MoonFrogEntity.java
+++ src/main/java/com/hauntedchest/lovecraftplus/entities/MoonFrogEntity.java
@@ -1,60 +1,24 @@
 package com.hauntedchest.lovecraftplus.entities;
 
 import com.hauntedchest.lovecraftplus.registries.EntityTypeHandler;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.fish.AbstractFishEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 @SuppressWarnings("NullableProblems")
 public class MoonFrogEntity extends AnimalEntity {
-    private static final DataParameter<Boolean> FROM_BUCKET = EntityDataManager.createKey(AbstractFishEntity.class, DataSerializers.BOOLEAN);
-
 
     public MoonFrogEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
         super(type, worldIn);
     }
 
-    public boolean preventDespawn() {
-        return this.isFromBucket();
-    }
 
-    public boolean canDespawn(double distanceToClosestPlayer) {
-        return !this.isFromBucket() && !this.hasCustomName();
-    }
-
-    protected void registerData() {
-        super.registerData();
-        this.dataManager.register(FROM_BUCKET, false);
-    }
-
-    private boolean isFromBucket() {
-        return this.dataManager.get(FROM_BUCKET);
-    }
-
-    public void setFromBucket(boolean p_203706_1_) {
-        this.dataManager.set(FROM_BUCKET, p_203706_1_);
-    }
-
-    public void writeAdditional(CompoundNBT compound) {
-        super.writeAdditional(compound);
-        compound.putBoolean("FromBucket", this.isFromBucket());
-    }
 
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new SwimGoal(this));
@@ -73,44 +37,6 @@ public class MoonFrogEntity extends AnimalEntity {
         this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2F);
     }
 
-    @Override
-    public boolean processInteract(PlayerEntity player, Hand hand) {
-        ItemStack itemstack = player.getHeldItem(hand);
-        if (itemstack.getItem() == Items.WATER_BUCKET && this.isAlive()) {
-            this.playSound(SoundEvents.ITEM_BUCKET_FILL_FISH, 1.0F, 1.0F);
-            itemstack.shrink(1);
-            ItemStack itemstack1 = this.getFrogBucket();
-            this.setBucketData(itemstack1);
-            if (!this.world.isRemote) {
-                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayerEntity)player, itemstack1);
-            }
-
-            if (itemstack.isEmpty()) {
-                assert itemstack1 != null;
-                player.setHeldItem(hand, itemstack1);
-            } else {
-                assert itemstack1 != null;
-                if (!player.inventory.addItemStackToInventory(itemstack1)) {
-                    player.dropItem(itemstack1, false);
-                }
-            }
-
-            this.remove();
-            return true;
-        } else {
-            return super.processInteract(player, hand);
-        }
-    }
-    protected void setBucketData(ItemStack bucket) {
-        if (this.hasCustomName()) {
-            bucket.setDisplayName(this.getCustomName());
-        }
-
-    }
-
-    private ItemStack getFrogBucket() {
-        return null;
-    }
 
     @Nullable
     @Override
