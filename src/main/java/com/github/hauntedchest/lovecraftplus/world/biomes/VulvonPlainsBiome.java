70cbede425c4c5f5b8636e5659d4d301a9665f61
DenDragon23 <61365614+DenDragon23@users.noreply.github.com>
2020-11-22 10:14:00 -1000

    There made it shorter

--- src/main/java/com/github/hauntedchest/lovecraftplus/world/biomes/VulvonPlainsBiome.java
+++ src/main/java/com/github/hauntedchest/lovecraftplus/world/biomes/VulvonPlainsBiome.java
@@ -18,11 +18,15 @@ import net.minecraft.world.gen.placement.Placement;
 @SuppressWarnings("NullableProblems")
 public class VulvonPlainsBiome extends Biome {
     private static final BlockState VINESWORD = BlockRegistries.CORRUPTED_VINESWORD.get().getDefaultState();
-    public static final BlockClusterFeatureConfig VINESWORD_CONFIG = (new BlockClusterFeatureConfig.Builder((new WeightedBlockStateProvider()).addWeightedBlockstate(VINESWORD, 2), new SimpleBlockPlacer())).tries(64).build();
+    public static final BlockClusterFeatureConfig VINESWORD_CONFIG = (new BlockClusterFeatureConfig.Builder((new WeightedBlockStateProvider()).addWeightedBlockstate(VINESWORD, 2), new SimpleBlockPlacer()).tries(64).build();
 
     public VulvonPlainsBiome(Biome.Builder builder) {
         super(builder);
-        this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_RANDOM_SELECTOR.withConfiguration(new MultipleWithChanceRandomFeatureConfig(ImmutableList.of(Feature.RANDOM_PATCH.withConfiguration(VINESWORD_CONFIG)), 2)).withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(new FrequencyConfig(5))));
+        this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+                Feature.RANDOM_RANDOM_SELECTOR.withConfiguration
+                        (new MultipleWithChanceRandomFeatureConfig(ImmutableList.of
+                                (Feature.RANDOM_PATCH.withConfiguration(VINESWORD_CONFIG)), 2))
+                        .withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(new FrequencyConfig(5))));
 
         DefaultBiomeFeatures.addStoneVariants(this);
         DefaultBiomeFeatures.addTaigaRocks(this);
