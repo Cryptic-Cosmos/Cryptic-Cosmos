90b39854a0746b801a8bcce86d2f639838148f59
DenDragon23 <denyswriter@gmail.com>
2021-01-07 22:22:46 -0800

    ANIMATION POGCHAMP

--- src/main/java/com/github/hauntedchest/lovecraftplus/client/entity/model/MoonBeastModel.java
+++ src/main/java/com/github/hauntedchest/lovecraftplus/client/entity/model/MoonBeastModel.java
@@ -2,13 +2,19 @@ package com.github.hauntedchest.lovecraftplus.client.entity.model;
 
 import com.github.hauntedchest.lovecraftplus.LovecraftPlus;
 import com.github.hauntedchest.lovecraftplus.entities.MoonBeastEntity;
+import com.github.hauntedchest.lovecraftplus.registries.Utils;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
+
+import javax.annotation.Nullable;
 
 public class MoonBeastModel extends AnimatedGeoModel<MoonBeastEntity> {
     @Override
     public ResourceLocation getModelLocation(MoonBeastEntity moonBeastEntity) {
-        return new ResourceLocation(LovecraftPlus.MOD_ID, "geo/MoonBeastModel.geo.json");
+        return new ResourceLocation(LovecraftPlus.MOD_ID, "geo/moonbeastmodel.geo.json");
     }
 
     @Override
@@ -18,6 +24,17 @@ public class MoonBeastModel extends AnimatedGeoModel<MoonBeastEntity> {
 
     @Override
     public ResourceLocation getAnimationFileLocation(MoonBeastEntity moonBeastEntity) {
-        return new ResourceLocation(LovecraftPlus.MOD_ID, "animations/MoonBeastModel.animation.json");
+        return new ResourceLocation(LovecraftPlus.MOD_ID, "animations/moonbeast.json");
+    }
+
+    @Override
+    public void setLivingAnimations(MoonBeastEntity entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        EntityModelData data = (EntityModelData) customPredicate.getExtraData().get(0);
+
+        // Apply head look to model
+        IBone head = this.getAnimationProcessor().getBone("head");
+        head.setRotationY((float) Math.toRadians(Utils.clamp(data.netHeadYaw, -45, 45)));
+        head.setRotationX(-(float) Math.toRadians(data.headPitch));
     }
 }
--- src/main/java/com/github/hauntedchest/lovecraftplus/entities/MoonBeastEntity.java
+++ src/main/java/com/github/hauntedchest/lovecraftplus/entities/MoonBeastEntity.java
@@ -34,14 +34,14 @@ public class MoonBeastEntity extends MonsterEntity implements IAnimatable {
     protected void registerGoals() {
         super.registerGoals();
 
-        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 3.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
         this.applyEntityAI();
     }
 
     protected void applyEntityAI() {
         this.goalSelector.addGoal(4, new SwimGoal(this));
-        this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.0D, false));
+        this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.5D, false));
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setCallsForHelp());
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
         this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, EndermanEntity.class, true));
@@ -52,8 +52,8 @@ public class MoonBeastEntity extends MonsterEntity implements IAnimatable {
         super.registerAttributes();
 
         this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(50.0D);
-        this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.5D);
-        this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(6.0D);
+        this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(1.75D);
+        this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.5D);
     }
 
     @Override
