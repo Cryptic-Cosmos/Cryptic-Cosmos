d6031860bb23631af4b1619b0fcc40a670449157
TheOnlyTails <theonlytails@theonlytails.com>
2021-06-26 02:42:36 +0300

    Made the rift command use the actual dimension's name.

--- src/main/java/com/crypticcosmos/crypticcosmos/command/RiftCommand.java
+++ src/main/java/com/crypticcosmos/crypticcosmos/command/RiftCommand.java
@@ -1,35 +1,34 @@
 package com.crypticcosmos.crypticcosmos.command;
 
 import com.crypticcosmos.crypticcosmos.block.RiftBlock;
-import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nonnull;
+
 import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
 import static org.apache.commons.text.WordUtils.capitalizeFully;
 
-public class RiftCommand implements Command<CommandSource> {
-    private static final RiftCommand INSTANCE = new RiftCommand();
-
-    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+public class RiftCommand {
+    public static void register(@Nonnull CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(literal("rift")
                 .requires((commandSource) -> commandSource.hasPermission(2))
+                // handles command if no player specified
+                .executes(context -> riftCommand(context, context.getSource().getPlayerOrException()))
                 .then(argument("target", EntityArgument.player())
-                        .executes(INSTANCE))
+                        // handles command if player specified
+                        .executes(context -> riftCommand(context, EntityArgument.getPlayer(context, "target")))
+                )
         );
     }
 
-    @Override
-    public int run(CommandContext<CommandSource> context) throws CommandSyntaxException {
-        final ServerPlayerEntity player = EntityArgument.getPlayer(context, "target");
-
+    public static int riftCommand(CommandContext<CommandSource> context, ServerPlayerEntity player) {
         ServerWorld destination = RiftBlock.rift(player.level, player, player.blockPosition());
         // gets the dimension's name
         final String destinationName = capitalizeFully(destination.dimension().location().getPath().replaceAll("_", " "), ' ', '_');
